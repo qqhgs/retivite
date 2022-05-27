311f8fd80289dc63e67129bcbbe078bf88f244ce
Ilham Bahtiar <bahtiari073@gmail.com>
2022-05-27 09:21:21 +0800

    rename variabel state name

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,38 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App: React.FC = () => {
-  const [darkMode, setDarkMode] = useState(false)
+  const [theme, setTheme] = useState(false)
 
   useEffect(() => {
-    const mode = localStorage.getItem('dark-mode')
-    mode !== 'dark' ? setDarkMode(false) : setDarkMode(true)
+    const mode = localStorage.getItem('theme')
+    mode !== 'dark' ? setTheme(false) : setTheme(true)
   })
 
   useEffect(() => {
-    if (darkMode) {
+    if (theme) {
       document.documentElement.classList.add('dark')
     } else {
       document.documentElement.classList.remove('dark')
     }
-  }, [darkMode])
+  }, [theme])
 
-  const handleDarkMode = () => {
-    if (darkMode) {
-      localStorage.setItem('dark-mode', 'light')
-      setDarkMode(false)
+  const handleTheme = () => {
+    if (theme) {
+      localStorage.setItem('theme', 'light')
+      setTheme(false)
     } else {
-      localStorage.setItem('dark-mode', 'dark')
-      setDarkMode(true)
+      localStorage.setItem('theme', 'dark')
+      setTheme(true)
     }
   }
 
   return (
-    <Box className='App'>
+    <Box>
       <VStack h='100vh' justifyContent='center'>
 				<Image className='logo' boxSize='80px' src='/logo.svg' alt='react'/>
         <Heading as='h1'>Good luck, have fun!</Heading>
         <Text>[ "React", "TypeScript", "Vite", "Chakra UI" ]</Text>
-				<Switch size='lg' onChange={handleDarkMode} isChecked={darkMode} />
+				<Switch size='lg' onChange={handleTheme} isChecked={theme} />
       </VStack>
     </Box>
   )
